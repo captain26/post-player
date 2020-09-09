4200195b53e9d894533265d2936461f5e3747fa0
Vaibhav <vaibhav.agarwal.eee19@itbhu.ac.in>
2020-09-09 15:35:29 +0530

    photo functionality done

--- lib/screens/add_photo.dart
+++ lib/screens/add_photo.dart
@@ -1,14 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:post_player/screens/drawer_screen.dart';
+import 'dart:io';
+import 'dart:math';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+String _caption;
 
 class AddPhotoPage extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: AppBar(
-          title: Text("Add Photo Page"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Photo Page"),
+      ),
+      drawer: DrawerScreen(),
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
         ),
-        drawer: DrawerScreen(),
-        body: Center(child: Text("This is Add photo page")));
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Caption',
+                  contentPadding:
+                      EdgeInsets.only(top: 25, bottom: 25, left: 20),
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.8),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(width: 0, color: Colors.black),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(width: 2, color: Colors.blue),
+                  ),
+                ),
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Caption is Required';
+                  }
+                  return null;
+                },
+                onSaved: (String value) {
+                  _caption = value;
+                },
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  if (!_formKey.currentState.validate()) {
+                    return;
+                  }
+                  _formKey.currentState.save();
+                  getImage();
+                },
+                child: Text('Upload Photo'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Future getImage() async {
+  // Get image from gallery.
+  // ignore: deprecated_member_use
+  var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+  _uploadImageToFirebase(image);
+}
+
+Future<void> _uploadImageToFirebase(File image) async {
+  try {
+    // Make random image name.
+    int randomNumber = Random().nextInt(10000000);
+    String imageLocation = 'images/image$randomNumber.jpg';
+    // Upload image to firebase.
+    final StorageReference storageReference =
+        FirebaseStorage().ref().child(imageLocation);
+    final StorageUploadTask uploadTask = storageReference.putFile(image);
+    await uploadTask.onComplete;
+    _addPathToDatabase(imageLocation);
+  } catch (e) {
+    print(e.message);
+  }
+}
+
+Future<void> _addPathToDatabase(String text) async {
+  try {
+    // Get image URL from firebase
+    final ref = FirebaseStorage().ref().child(text);
+    var imageString = await ref.getDownloadURL();
+    await Firestore.instance.collection('images').document().setData({
+      'url': imageString,
+      'location': text,
+      'caption': _caption,
+      'createdOn': FieldValue.serverTimestamp()
+    });
+  } catch (e) {
+    print(e.message);
+    showDialog(
+        builder: (context) {
+          return AlertDialog(
+            content: Text(e.message),
+          );
+        },
+        context: null);
   }
 }
